5d90470c80b5bbdee017a9e0c828a063ff33fc95
Cody Cutrer <cody@instructure.com>
2015-07-27 18:17:14 -0600

    shoot something!

--- tank.rb
+++ tank.rb
@@ -1,7 +1,9 @@
-#!/usr/bin/env
+#!/usr/bin/env ruby
 
 require 'net/http'
 require 'json'
+require 'byebug'
+require 'uri'
 
 module Utils
   def self.infer_orientation(last_position, position, distance)
@@ -30,6 +32,13 @@ module Utils
       [position.first - distance, position.last]
     end
   end
+
+  def self.perpendicular?(orientation1, orientation2)
+    ((orientation1 == 'N' || orientation1 == 'S') &&
+        (orientation2 == 'E' || orientation2 == 'W')) ||
+      ((orientation1 == 'E' || orientation1 == 'W') &&
+          (orientation2 == 'N' || orientation2 == 'S'))
+  end
 end
 
 class Laser
@@ -37,17 +46,16 @@ class Laser
 
   SPEED = 2
 
-  def initialize(position)
+  def initialize(position, tank1_position, tank1_orientation, tank2_position, tank2_orientation)
     @position = position
+    @orientation = tank1_orientation if tank1_orientation && position == Utils.project_move(tank1_position, tank1_orientation, SPEED)
+    @orientation ||= tank2_orientation if tank2_orientation && position == Utils.project_move(tank2_position, tank2_orientation, SPEED)
+    @orientation ||= Utils.infer_orientation(tank1_position, position, SPEED)
+    @orientation ||= Utils.infer_orientation(tank2_position, position, SPEED)
   end
 
   def matches?(position)
-    if !orientation
-      if (@orientation = Utils.infer_orientation(@position, position, SPEED))
-      else
-        return false
-      end
-    elsif (position == Utils.project_move(@position, orientation, SPEED))
+    unless (position == Utils.project_move(@position, orientation, SPEED))
       return false
     end
     @position = position
@@ -56,14 +64,13 @@ class Laser
 end
 
 class Tank
-  attr_reader :last_orientation, :position, :orientation_current
+  attr_accessor :orientation, :position, :inferred_orientation
 
   SPEED = 1
 
   def update(position)
-    orientation = Utils.infer_orientation(@position, position, SPEED)
-    @last_orientation = orientation || @last_orientation
-    @orientation_current = !!orientation
+    @orientation = Utils.infer_orientation(@position, position, SPEED) if @position
+    @inferred_orientation = @orientation || @inferred_orientation
     @position = position
   end
 end
@@ -72,12 +79,14 @@ class Game
   attr_reader :state
 
   def initialize(host, id, me)
-    @host, @id = host, id
-    @http = Net::HTTP.new(host)
-    join(me)
+    @host, @id = URI.parse(host), id
+    @http = Net::HTTP.new(@host.host, @host.port)
     @lasers = []
     @batteries = []
     @opponent = Tank.new
+    @me = Tank.new
+    @moves = 0
+    join(me)
   end
 
   def join(me)
@@ -94,25 +103,35 @@ class Game
     @prior_lasers = @lasers
     @lasers = []
     @batteries = []
+    @me.orientation = @state['orientation'][0].upcase
+    @opponent_last_spot = @opponent.position
+    @opponent_last_orientation = @opponent.orientation
+    @my_last_spot = @me.position
+    @my_last_orientation = @me.orientation
     @board.each_with_index do |row, y|
-      @board.chars.each_with_index do |c, x|
+      row.each_char.each_with_index do |c, x|
         pos = [x, y]
         case c
         when 'X'
-          @me = pos
+          @me.position = pos
         when 'O'
           @opponent.update(pos)
         when 'L'
           if (laser = @lasers.find { |l| l.matches?(pos) } )
             @lasers << laser
           else
-            @lasers << Laser.new(pos)
+            @lasers << Laser.new(pos, @opponent_last_spot, @opponent_last_orientation, @my_last_spot, @my_last_orientation)
           end
         when 'B'
           @batteries << pos
         end
       end
     end
+    puts @board
+    puts @me.inspect
+    puts @opponent.inspect
+    puts @lasers.inspect
+    puts @batteries.inspect
   end
 
   def running?
@@ -121,6 +140,7 @@ class Game
 
   def make_move(action)
     puts action
+    puts "=" * 40
     req = Net::HTTP::Post.new(@host.merge("/game/#{@id}/#{action}"))
     req['X-Sm-Playerid'] = @player_id
     res = @http.request(req)
@@ -128,7 +148,12 @@ class Game
   end
 
   def move
-    make_move('noop')
+    @moves += 1
+    if @moves == 1
+      make_move('fire')
+    else
+      make_move('noop')
+    end
   end
 end
 
